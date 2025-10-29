bf78a427e3ac98a35037990330ed10c32866f07c
esumerinde <esumerinde@gmail.com>
2025-10-29 01:55:31 -0300

    refactor: simplifica LoginLayout y LTSimpleHeader solo logo minimal

--- src/components/Layout/LoginLayout/LoginLayout.jsx
+++ src/components/Layout/LoginLayout/LoginLayout.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import LTSimpleHeader from "../Header/LTSimpleHeader";
-import { useIsMobile } from "../../../hooks/useIsMobile";
-import "../Header/LTHeader.onlylogo.css";
 
 /**
  * Layout para páginas de autenticación (Login, Register)
  * Solo muestra SimpleHeader (logo únicamente) sin navbar ni footer
  */
-const LoginLayout = () => {
-  const isMobile = useIsMobile();
-
-  return (
-    <div className="lt-app">
-      <LTSimpleHeader />
-      <Outlet />
-    </div>
-  );
-};
+const LoginLayout = () => (
+  <div className="lt-app">
+    <LTSimpleHeader />
+    <Outlet />
+  </div>
+);
 
 export default LoginLayout;
